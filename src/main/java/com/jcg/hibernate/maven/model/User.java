b9e2693f3c9901a618924dc35d671ccf92497064
H P <H P@LAPTOP-KASON>
2026-02-28 20:00:46 +0700

    update

--- src/main/java/com/jcg/hibernate/maven/model/User.java
+++ src/main/java/com/jcg/hibernate/maven/model/User.java
@@ -4,20 +4,21 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import org.hibernate.validator.constraints.Length;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.Accessors;
 
 @Builder(toBuilder=true) //เพื่อให้สามารถสร้าง object ใหม่ได้จาก object เดิมด้วยคำสั่ง objOld.toBuilder().build(); เป็นการ clone มาเป็นตัวใหม่
 @Accessors(chain=true)  //ทำให้ใช้คำสั่ง User mm = new User().setUserid(1).setUsername("kk"); ได้
 @AllArgsConstructor //สร้าง method Constructor แบบมี Argument ทุก member
 @NoArgsConstructor  //สร้าง method Constructor แบบไม่มี Argument
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "user_table")
 public class User  {
@@ -27,7 +28,7 @@ public class User  {
 	private int userid;
 
 	@Column(name = "user_name", length = 255)
-	@Length(min = 1, max = 255, message = "ความยาวของชื่อสุดท้ายต้องอยู่ระหว่าง 1 ถึง 255 ตัวอักษร")
+	@Size(min = 1, max = 255, message = "ความยาวของชื่อสุดท้ายต้องอยู่ระหว่าง 1 ถึง 255 ตัวอักษร")
 	private String username;
 	
 	@Column(name = "birth_day")
